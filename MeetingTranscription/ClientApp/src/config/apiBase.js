c2bc97c60fe2cf78ecb56afcfe1055df31e05e4d
Jiabin <v-jiabinzhou@microsoft.com>
2026-03-28 21:46:41 +0800

    Fix USE_BYTE_DANCE logic and improve formatting for APPINSIGHTS_CONNECTION_STRING in apiBase.js

--- MeetingTranscription/ClientApp/src/config/apiBase.js
+++ MeetingTranscription/ClientApp/src/config/apiBase.js
@@ -5,7 +5,7 @@ const runtimeEnv = (typeof window !== 'undefined' && window.__env) || {};
 
 export const API_BASE = runtimeEnv.REACT_APP_API_BASE || process.env.REACT_APP_API_BASE || '';
 
-export const USE_BYTE_DANCE = (runtimeEnv.REACT_APP_USE_BYTE_DANCE || process.env.REACT_APP_USE_BYTE_DANCE) === 'true'
-    || (runtimeEnv.REACT_APP_USE_BYTE_DANCE || process.env.REACT_APP_USE_BYTE_DANCE) === true;
+export const USE_BYTE_DANCE = (runtimeEnv.REACT_APP_USE_BYTE_DANCE || process.env.REACT_APP_USE_BYTE_DANCE || 'true') === 'true';
 
-export const APPINSIGHTS_CONNECTION_STRING = runtimeEnv.REACT_APP_APPINSIGHTS_CONNECTION_STRING || process.env.REACT_APP_APPINSIGHTS_CONNECTION_STRING || 'InstrumentationKey=ac6d0468-ba88-4aa2-99ab-b41ec0fc6cf7;IngestionEndpoint=https://eastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://eastasia.livediagnostics.monitor.azure.com/;ApplicationId=874cda30-af64-4dd9-95c3-f78eb851d4a6';
+export const APPINSIGHTS_CONNECTION_STRING = runtimeEnv.REACT_APP_APPINSIGHTS_CONNECTION_STRING
+    || process.env.REACT_APP_APPINSIGHTS_CONNECTION_STRING || 'InstrumentationKey=ac6d0468-ba88-4aa2-99ab-b41ec0fc6cf7;IngestionEndpoint=https://eastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://eastasia.livediagnostics.monitor.azure.com/;ApplicationId=874cda30-af64-4dd9-95c3-f78eb851d4a6';
