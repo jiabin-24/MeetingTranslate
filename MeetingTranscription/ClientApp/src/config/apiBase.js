1ad8b9c2e6495a275b9e6aa42ab2381b62020297
Jiabin <v-jiabinzhou@microsoft.com>
2026-04-18 10:42:53 +0800

    Update Application Insights connection string in apiBase.js

--- MeetingTranscription/ClientApp/src/config/apiBase.js
+++ MeetingTranscription/ClientApp/src/config/apiBase.js
@@ -8,4 +8,4 @@ export const API_BASE = runtimeEnv.REACT_APP_API_BASE || process.env.REACT_APP_A
 export const USE_BYTE_DANCE = (runtimeEnv.REACT_APP_USE_BYTE_DANCE || process.env.REACT_APP_USE_BYTE_DANCE || 'true') === 'true';
 
 export const APPINSIGHTS_CONNECTION_STRING = runtimeEnv.REACT_APP_APPINSIGHTS_CONNECTION_STRING
-    || process.env.REACT_APP_APPINSIGHTS_CONNECTION_STRING || 'InstrumentationKey=ac6d0468-ba88-4aa2-99ab-b41ec0fc6cf7;IngestionEndpoint=https://eastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://eastasia.livediagnostics.monitor.azure.com/;ApplicationId=874cda30-af64-4dd9-95c3-f78eb851d4a6';
+    || process.env.REACT_APP_APPINSIGHTS_CONNECTION_STRING || 'InstrumentationKey=417b3b95-89c4-4cce-99d0-4d6ea191f711;IngestionEndpoint=https://eastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://eastasia.livediagnostics.monitor.azure.com/;ApplicationId=fd32a292-b97b-487d-b94e-dede5b003c4e';
